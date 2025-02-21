a36c006f3f6243c999ecc4f77257db7492b9e896
Hayo87 <98052250+Hayo87@users.noreply.github.com>
2025-02-21 10:59:12 +0100

    rename application

--- backend/src/main/java/io/github/Hayo87/AutomataExplorerApplication.java
+++ backend/src/main/java/io/github/Hayo87/AutomataExplorerApplication.java
@@ -1,13 +1,14 @@
 package io.github.Hayo87;
 
+import org.checkerframework.checker.units.qual.A;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class VisDiffBackendApplication {
+public class AutomataExplorerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(VisDiffBackendApplication.class, args);
+		SpringApplication.run(AutomataExplorerApplication.class, args);
 	}
 
 }
