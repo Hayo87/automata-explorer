bd165aaebba04559d7f2fc11be2029377257e4ec
Hayo87 <98052250+Hayo87@users.noreply.github.com>
2025-02-25 09:32:47 +0100

    removed unused import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import UploadPage from './pages/UploadPage';
 import VisualizationPage from './pages/VisualizationPage';
