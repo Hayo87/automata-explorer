e02fd67970e1f269f51dc24b2d74dad65bf03d0e
Hayo87 <98052250+Hayo87@users.noreply.github.com>
2025-03-11 10:58:13 +0100

    add sidebar

--- frontend/src/pages/VisualizationPage.tsx
+++ frontend/src/pages/VisualizationPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import CytoscapeVisualization from '../components/CytoscapeVisualization';
 import { useSession } from '../hooks/useSession';
@@ -7,6 +7,7 @@ import '../index.css';
 const VisualizationPage: React.FC = () => {
   const { sessionId } = useParams<{ sessionId: string }>();
   const { data, loadSessionData, loading } = useSession();
+  const [isCollapsed, setIsCollapsed] = useState(false);
 
   useEffect(() => {
     if (sessionId) {
@@ -15,15 +16,34 @@ const VisualizationPage: React.FC = () => {
   }, [sessionId]);
 
   return (
-    <div className="vis-container">
+    <div className="page-container">
       <img src="/logo.svg" alt="Logo" className="small-logo" />
-      {loading ? (
-        <p>Loading visualization...</p>
-      ) : data ? (
-        <CytoscapeVisualization data={data} />
-      ) : (
-        <p>No data available</p>
-      )}
+
+      <div className="content-container">
+        <main className="graph-area">
+          {loading ? (
+            <p>Loading visualization...</p>
+          ) : data ? (
+            <CytoscapeVisualization data={data} />
+          ) : (
+            <p>No data available</p>
+          )}
+        </main>
+
+        <aside className="sidebar">
+          <button className="sidebar-button"  title="Exit this visualization"> <span className="material-icons">exit_to_app</span> </button>
+          <hr></hr>
+          <button className="sidebar-button" title= "Set dot layout"> <span className="material-icons">more_horiz</span> </button>
+          <button className="sidebar-button" title = "Set circular layout"> <span className="material-icons">radio_button_unchecked</span> </button>
+          <button className="sidebar-button" title = "Set grid layout"> <span className="material-icons">grid_view</span> </button>
+          <hr></hr>
+          <button className="sidebar-button" title= "Filter"> <span className="material-icons">filter_alt</span> </button>
+          <hr></hr>
+          <button className="sidebar-button" title= "Get input information"> <span className="material-icons">info</span> </button>
+        </aside>
+
+
+      </div>
     </div>
   );
 };
