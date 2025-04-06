0460f1ed0377fc2e93664043caa4efb4d3844c26
Hayo87 <98052250+Hayo87@users.noreply.github.com>
2025-04-06 22:33:55 +0200

    change options

--- frontend/src/utils/attachContextCollapse.tsx
+++ frontend/src/utils/attachContextCollapse.tsx
@@ -1,12 +1,11 @@
-// expandCollapseUtil.ts
 import cytoscape from 'cytoscape';
 
 export function attachExpandCollapse(cyInstance: cytoscape.Core) {
     const defaultOptions = {
-      animate: false,
+      animate: true,
       animationDuration: 500,
-      undoable: false,
-      cueEnabled: false,
+      undoable: true,
+      cueEnabled: true,
     };
   
     return cyInstance.expandCollapse(defaultOptions);
