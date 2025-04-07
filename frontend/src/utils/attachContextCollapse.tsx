79f9851081a6948ad13092ff41f4c8765f4a473d
Hayo87 <98052250+Hayo87@users.noreply.github.com>
2025-04-07 10:29:06 +0200

    change defaults

--- frontend/src/utils/attachContextCollapse.tsx
+++ frontend/src/utils/attachContextCollapse.tsx
@@ -1,12 +1,15 @@
 import cytoscape from 'cytoscape';
 
 export function attachExpandCollapse(cyInstance: cytoscape.Core) {
-    const defaultOptions = {
-      animate: true,
-      animationDuration: 500,
-      undoable: true,
-      cueEnabled: true,
-    };
+    const defaultOptions = { animate: true,
+    animationDuration: 500,
+    undoable: true,
+    cueEnabled: true,
+    expandCollapseCuePosition: 'top-left',
+    expandCollapseCueSize: 12,
+    expandCollapseCueLineSize: 2,
+    groupEdgesOfSameTypeOnCollapse: true
+  }
   
     return cyInstance.expandCollapse(defaultOptions);
   }
