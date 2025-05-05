0b2b53e9a84a0bcdc851d0e8710825a4e6f3e2b6
Hayo87 <98052250+Hayo87@users.noreply.github.com>
2025-05-05 14:07:46 +0200

    simple Mealy

--- backend/src/main/java/io/github/Hayo87/model/MealyTransition/Mealy.java
+++ backend/src/main/java/io/github/Hayo87/model/MealyTransition/Mealy.java
@@ -1,44 +1,18 @@
 package io.github.Hayo87.model.MealyTransition;
 
-import com.github.tno.gltsdiff.glts.lts.automaton.diff.DiffKind;
-import com.github.tno.gltsdiff.glts.lts.automaton.diff.DiffProperty;
+import io.github.Hayo87.model.Utils.LabelUtils;
+/**
+ * Represents a Mealy transition with an input and an output.
+ * 
+ * <p> This record can be used a transition property in {@code DiffAutomaton<Mealy>} </p>
+ * 
+ * @param input the input for the transition
+ * @param output, the output for the transition
+ */
+public record Mealy(String input, String output) {
 
-public class Mealy {
-    private final DiffProperty<String> input;
-    private final DiffProperty<String> output;
-    private final DiffProperty<String> additionalInput;
-    private final boolean dual;
-
-    /**
-     * Constructor to create a Mealy transition property with a single input and output
-     * @param input
-     * @param output
-     * @param diffkind
-     */
-    public Mealy(String input, String output, DiffKind diffkind) {
-        this.input = new DiffProperty<>(input, diffkind);
-        this.output = new DiffProperty<>(output, diffkind); 
-        this.additionalInput = null;
-        this.dual = false;
+    @Override
+    public String toString() {
+        return LabelUtils.build(input, output);
     }
-
-    /**
-     * Constructor to create  a Mealy transition property with multiple inputs.
-     * @param input
-     * @param output
-     * @param additionalInput
-     * @param dualKind
-     */
-    public Mealy(DiffProperty<String> input, DiffProperty<String> output, DiffProperty<String> additional) {
-        this.input = input;
-        this.output = output;
-        this.additionalInput = additional;
-        this.dual= true;
-    }
-
-    public DiffProperty<String> getInput() { return input; }
-    public DiffProperty<String> getOutput() { return output; }
-    public DiffProperty<String> getAdditionalInput() { return additionalInput; }
-    public boolean isDual() { return dual; }
 }
-   
--- backend/src/main/java/io/github/Hayo87/model/MealyTransition/MealyCombiner.java
+++ backend/src/main/java/io/github/Hayo87/model/MealyTransition/MealyCombiner.java
@@ -1,47 +1,18 @@
 package io.github.Hayo87.model.MealyTransition;
 
-import com.github.tno.gltsdiff.glts.lts.automaton.diff.DiffKind;
-import com.github.tno.gltsdiff.glts.lts.automaton.diff.DiffProperty;
 import com.github.tno.gltsdiff.operators.combiners.Combiner;
 import com.github.tno.gltsdiff.operators.combiners.EqualityCombiner;
-import com.github.tno.gltsdiff.operators.combiners.lts.automaton.diff.DiffKindCombiner;
-import com.github.tno.gltsdiff.operators.combiners.lts.automaton.diff.DiffPropertyCombiner;
 
-public class MealyCombiner extends Combiner<DiffProperty<Mealy>> {
+public class MealyCombiner extends Combiner<Mealy> {
 
-    private static final DiffPropertyCombiner<String> diffPropertyCombiner = new DiffPropertyCombiner<>(new EqualityCombiner<>(), new DiffKindCombiner());
     @Override
-    protected boolean computeAreCombinable(DiffProperty<Mealy> left, DiffProperty<Mealy> right) { 
-        Mealy lMealy = left.getProperty();
-        Mealy rMealy = right.getProperty();
-
-        boolean alreadyDual = lMealy.isDual() || rMealy.isDual();
-        boolean combinableInput = diffPropertyCombiner.areCombinable(lMealy.getInput(), rMealy.getInput());
-
-        return (!alreadyDual && combinableInput); 
+    protected boolean computeAreCombinable(Mealy left, Mealy right) {
+        EqualityCombiner<String> combiner = new EqualityCombiner<>();
+        return combiner.areCombinable(left.toString(), right.toString());
     }
-    
+
     @Override
-    protected DiffProperty<Mealy> computeCombination(DiffProperty<Mealy> left, DiffProperty<Mealy> right) {
-        Mealy lMealy = left.getProperty();
-        Mealy rMealy = right.getProperty();
-        Mealy combinedMealy; 
-
-        Boolean inputCombinable = diffPropertyCombiner.areCombinable(lMealy.getInput(), rMealy.getInput());
-        Boolean outputCombinable = diffPropertyCombiner.areCombinable(lMealy.getOutput(), rMealy.getOutput());
-
-        if (inputCombinable && outputCombinable ) {          
-            combinedMealy =  new Mealy( 
-                                lMealy.getInput().getProperty(), 
-                                lMealy.getOutput().getProperty(), 
-                                DiffKind.UNCHANGED);
-        }
-        else { // inputCombinable && !outputCombinable
-            combinedMealy = new Mealy( 
-                                new DiffProperty<>(lMealy.getInput().getProperty(), DiffKind.UNCHANGED),
-                                new DiffProperty<>(lMealy.getOutput().getProperty(), lMealy.getOutput().getDiffKind()),
-                                new DiffProperty<>(rMealy.getOutput().getProperty(), rMealy.getOutput().getDiffKind()));                
-        }
-        return new DiffProperty<>(combinedMealy, DiffKind.UNCHANGED);
+    protected Mealy computeCombination(Mealy left, Mealy right) {
+        return new Mealy(left.input(), left.output());
     }
 }
