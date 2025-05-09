d4ec32187d4de03db3bc5813649b36177592e074
Hayo87 <98052250+Hayo87@users.noreply.github.com>
2025-05-09 17:40:25 +0200

    match backend and exceptions

---
+++ frontend/src/api/RequestResponse.ts
@@ -0,0 +1,71 @@
+// Interfaces for sessions
+export interface SessionRequest{
+  type: "MEALY" | "STRING";
+  reference: string;
+  subject: string;
+}
+
+export interface SessionResponse {
+  sessionId: string;
+  processingOptions: ProcessOption[];
+}
+
+export interface ProcessOption {
+  stage:"PRE" | "POST"
+  type: string,
+  subtypes: string[]
+}
+
+// Interfaces for build settings and responses
+export interface BuildRequest{
+  actions?: ProcessAction[];
+}
+export interface BuildResponse  {
+  type: string;
+  build: Build;
+  actions: ProcessAction[];
+}
+
+export interface Build {
+  name: string; 
+  nodes: Node[];
+  edges: Edge[];
+}
+
+export interface Node {
+  id: number;
+  attributes?: NodeAttributes;
+}
+
+export interface NodeAttributes{
+  label: string;
+  isInitial: boolean;
+  diffkind: string;
+}
+
+export interface Edge {
+  id: string;
+  source: number;
+  target: number;
+  attributes?: EdgeAttributes;
+}
+
+export interface EdgeAttributes{
+  diffkind: string;
+  labeltext: string;
+  label: LabelEntry[];
+}
+export interface LabelEntry {
+  type: 'input' | 'output' | 'label';
+  value: string;
+  diffkind?: 'UNCHANGED' | 'ADDED' | 'REMOVED'; 
+}
+
+export interface ProcessAction {
+    stage: String,
+    type: string; 
+    subtype?: string;
+    order: number;
+    name?: string;  
+    values?: string[];
+}
